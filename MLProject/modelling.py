f22fe5b619e76c27bb589d1fb2b5dcb6ff8c8944
Kadek Dwi Putri Sunartini <147305841+dwisunartini@users.noreply.github.com>
2025-12-24 07:44:56 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,27 +5,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
 def train():
-    # Pastikan file dataset ada di root repositori Anda
+    # Load dataset
     df = pd.read_csv('examscore_preprocessing_automate.csv')
     X = df.drop(columns=['exam_score'])
     y = df['exam_score']
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # MLflow Project akan mengelola start_run otomatis
-    # Jangan gunakan mlflow.start_run() lagi di sini
+    # JANGAN gunakan mlflow.start_run() di sini. 
+    # MLflow Project yang akan mengelolanya secara otomatis.
     model = RandomForestRegressor(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
     
-    # Log metrik dan model
+    # Log metrik dan model ke dalam run yang sudah dibuat otomatis
     score = model.score(X_test, y_test)
     mlflow.log_metric("r2_score", score)
     mlflow.sklearn.log_model(model, "model")
     
-    print(f"Training selesai dengan R2 Score: {score}")
+    print(f"Training selesai. R2 Score: {score}")
 
 if __name__ == "__main__":
-    train()train()
+    train()train()train()
 
 
 
